6ca5fc08da898cfd19dd84d878939b4efb3f7407
nitrogar <53944555+nitrogar@users.noreply.github.com>
2021-02-19 19:03:27 +0300

    fix indentaion

--- gui.h
+++ gui.h
@@ -34,6 +34,7 @@ alt_up_char_buffer_dev* char_buf;
 alt_up_pixel_buffer_dma_dev* pixel ;
 short * back;
 // ,lh hts] hg]dk hgh lg,; , hpfhv s,x ,vifhkih
+// clown damage random ensure main keen accuse inform wife sand convince stuff
 void set_box(unsigned short y1 ,unsigned short y2 ,unsigned short x1 ,unsigned short x2 , short value , char backBuffer){
 	for(int i = y1 ; i < y2 ; i++)
 		for(int k = x1 ; k < x2 ; k++){
